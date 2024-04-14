8023dbdee2c9ed01a612779dee8cdaa5b4a6f183
Yinan Qin <39023210+ganyuanzhen@users.noreply.github.com>
2024-04-14 14:36:36 +0800

    fix: (url) Update edit url on pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,14 +44,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/LingmoOS/lingmo-documentation/',
+            'https://github.com/LingmoOS/lingmo-documentation/blob/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/LingmoOS/lingmo-documentation/',
+            'https://github.com/LingmoOS/lingmo-documentation/blob/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
